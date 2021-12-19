af91540253fec9046fd0a9667f99d1daf235eaa1
João Ceia <joao.p.ceia@gmail.com>
2021-12-19 01:09:59 +0000

    added definition of copy constructor and assignment operator overload in Animal class

--- ex00/src/Animal.cpp
+++ ex00/src/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 23:39:48 by jceia             #+#    #+#             */
-/*   Updated: 2021/12/19 00:10:46 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/19 01:02:11 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,26 @@ Animal::Animal(const std::string& type)
     #endif
 }
 
+Animal::Animal(const Animal& rhs)
+    : _type(rhs._type)
+{
+    #ifdef DEBUG
+    std::cout << "Animal copy constructor called" << std::endl;
+    #endif
+}
+
 Animal::~Animal()
 {
     #ifdef DEBUG
     std::cout << "Animal destructor called" << std::endl;
     #endif
+}
+
+Animal& Animal::operator=(const Animal& rhs)
+{
+    if (this != &rhs)
+        _type = rhs._type;
+    return *this;
 }
 
 std::string Animal::getType(void) const
