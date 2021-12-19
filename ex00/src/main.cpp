8ee6c01f77e99f66f656437c0515200840f4b69d
João Ceia <joao.p.ceia@gmail.com>
2021-12-19 00:27:43 +0000

    added test for wrongCat

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,26 +6,47 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 23:47:27 by jceia             #+#    #+#             */
-/*   Updated: 2021/12/15 01:25:50 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/19 00:25:18 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main(void)
 {
-    const Animal* meta = new Animal();
-    const Animal* dog = new Dog();
-    const Animal* cat = new Cat();
-    std::cout << dog->getType() << " " << std::endl;
-    std::cout << cat->getType() << " " << std::endl;
-    dog->makeSound();
-    cat->makeSound(); // will output the cat sound!
-    meta->makeSound();
+    {
+        // 'Correct' solution
+        const Animal* meta = new Animal();
+        const Animal* dog = new Dog();
+        const Animal* cat = new Cat();
 
-    delete meta;
-    delete dog;
-    delete cat;
+        std::cout << dog->getType() << " " << std::endl;
+        std::cout << cat->getType() << " " << std::endl;
+
+        std::cout << "Dog sound: ";
+        dog->makeSound();
+        std::cout << "Cat sound: ";
+        cat->makeSound(); // will output the cat sound!
+        std::cout << "Animal sound: ";
+        meta->makeSound();
+
+        delete meta;
+        delete dog;
+        delete cat;
+    }
+    {
+        // 'Incorrect' solution
+        const WrongAnimal* cat = new WrongCat();
+
+        std::cout << cat->getType() << " " << std::endl;
+
+        std::cout << "WrongCat sound: ";
+        cat->makeSound(); // will NOT output the cat sound!
+
+        delete cat;
+    }
 }
