ce49918b8f577b47a7679256a4d0bf417b28ff4e
João Ceia <joao.p.ceia@gmail.com>
2021-12-20 01:54:04 +0000

    extended ex00 tests

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 23:47:27 by jceia             #+#    #+#             */
-/*   Updated: 2021/12/19 00:25:18 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/20 01:52:57 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ int main(void)
         const Animal* dog = new Dog();
         const Animal* cat = new Cat();
 
-        std::cout << dog->getType() << " " << std::endl;
-        std::cout << cat->getType() << " " << std::endl;
+        std::cout << "type: " << dog->getType() << " " << std::endl;
+        std::cout << "type: " << cat->getType() << " " << std::endl;
 
         std::cout << "Dog sound: ";
         dog->makeSound();
@@ -42,10 +42,21 @@ int main(void)
         // 'Incorrect' solution
         const WrongAnimal* cat = new WrongCat();
 
-        std::cout << cat->getType() << " " << std::endl;
+        std::cout << "type: " << cat->getType() << " " << std::endl;
 
-        std::cout << "WrongCat sound: ";
+        std::cout << "WrongCat sound (being WrongAnimal ptr): ";
         cat->makeSound(); // will NOT output the cat sound!
+
+        delete cat;
+    }
+    {
+        // 'Incorrect' solution
+        const WrongCat* cat = new WrongCat();
+
+        std::cout << "type: " << cat->getType() << " " << std::endl;
+
+        std::cout << "WrongCat sound (being WrongCat ptr): ";
+        cat->makeSound();
 
         delete cat;
     }
